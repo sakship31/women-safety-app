c6b7798686f4321bb9c03dd19926f4cf9eda8fc9
tarush-r <tarush.r@somaiya.edu>
2021-09-19 15:38:53 +0530

    SOS done

--- lib/screens/sos.dart
+++ lib/screens/sos.dart
@@ -56,125 +56,140 @@ class _SOSPageState extends State<SOSPage> {
               padding: EdgeInsets.all(30),
               child: Container(
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
                     // SizedBox(height: 100),
                     Row(
-                      children: [InkWell(
-                        onTap: () async {
-                          Position position = await getCurrentLocation();
-                          print("Pressed");
-                          contacts = await dbHelper.getContacts();
-                          print(contacts[0].number);
-                          SmsSender sender = new SmsSender();
-                          for (int i = 0; i < contacts.length; i++) {
-                            print(i);
-                            await sender.sendSms(new SmsMessage(
-                                contacts[i].number.trim(),
-                                'I am safe ' +
-                                    'http://maps.google.com/?q=' +
-                                    position.latitude.toString() +
-                                    ',' +
-                                    position.longitude.toString()));
-                          }
-                        },
-                        child: Container(
-                          height: 150,
-                          width: 150,
-                          decoration: BoxDecoration(
-                              border: Border.all(color: Colors.white, width: 5),
-                              borderRadius: BorderRadius.circular(300),
-                              color: Colors.green[600]),
-                          child: Center(
-                            child: Container(
-                              child: Text("Safe",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 25,
-                                  )),
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        InkWell(
+                          onTap: () async {
+                            Position position = await getCurrentLocation();
+                            print("Pressed");
+                            contacts = await dbHelper.getContacts();
+                            print(contacts[0].number);
+                            SmsSender sender = new SmsSender();
+                            for (int i = 0; i < contacts.length; i++) {
+                              print(i);
+                              await sender.sendSms(
+                                new SmsMessage(
+                                  contacts[i].number.trim(),
+                                  'I am safe ' +
+                                      'http://maps.google.com/?q=' +
+                                      position.latitude.toString() +
+                                      ',' +
+                                      position.longitude.toString(),
+                                ),
+                              );
+                            }
+                            Navigator.of(context).pop();
+                          },
+                          child: Container(
+                            height: 150,
+                            width: 150,
+                            decoration: BoxDecoration(
+                                border:
+                                    Border.all(color: Colors.white, width: 5),
+                                borderRadius: BorderRadius.circular(300),
+                                color: Colors.green[600]),
+                            child: Center(
+                              child: Container(
+                                child: Text("Safe",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 25,
+                                    )),
+                              ),
                             ),
                           ),
                         ),
-                      ),]
+                      ],
                     ),
-                    
-                    Row(
-                      children:[InkWell(
-                        onTap: () async {
-                                                Position position = await getCurrentLocation();
-                        print("Pressed");
-                        contacts = await dbHelper.getContacts();
-                        print(contacts[0].number);
-                        SmsSender sender = new SmsSender();
-                        for (int i = 0; i < contacts.length; i++) {
-                          print(i);
-                          await sender.sendSms(new SmsMessage(
-                              contacts[i].number.trim(),
-                              'Being Cautious ' +
-                                  'http://maps.google.com/?q=' +
-                                  position.latitude.toString() +
-                                  ',' +
-                                  position.longitude.toString()));
-                        }
-                        },
 
-                        child: Container(
-                          height: 150,
-                          width: 150,
-                          decoration: BoxDecoration(
-                              border: Border.all(color: Colors.white, width: 5),
-                              borderRadius: BorderRadius.circular(300),
-                              color: Colors.yellow[600]),
-                          child: Center(
-                            child: Container(
-                              child: Text("Suspicious",
-                                  style: TextStyle(
-                                    color: Colors.black,
-                                    fontSize: 25,
-                                  )),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        InkWell(
+                          onTap: () async {
+                            Position position = await getCurrentLocation();
+                            print("Pressed");
+                            contacts = await dbHelper.getContacts();
+                            print(contacts[0].number);
+                            SmsSender sender = new SmsSender();
+                            for (int i = 0; i < contacts.length; i++) {
+                              print(i);
+                              await sender.sendSms(new SmsMessage(
+                                  contacts[i].number.trim(),
+                                  'Being Cautious ' +
+                                      'http://maps.google.com/?q=' +
+                                      position.latitude.toString() +
+                                      ',' +
+                                      position.longitude.toString()));
+                            }
+                            Navigator.of(context).pop();
+                          },
+                          child: Container(
+                            height: 150,
+                            width: 150,
+                            decoration: BoxDecoration(
+                                border:
+                                    Border.all(color: Colors.white, width: 5),
+                                borderRadius: BorderRadius.circular(300),
+                                color: Colors.yellow[600]),
+                            child: Center(
+                              child: Container(
+                                child: Text("Suspicious",
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 25,
+                                    )),
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    SizedBox(width: 20),
-                    SafeArea(
-                      child: InkWell(
-                        onTap: () async {
-                                                  Position position = await getCurrentLocation();
-                        print("Pressed");
-                        contacts = await dbHelper.getContacts();
-                        print(contacts[0].number);
-                        SmsSender sender = new SmsSender();
-                        for (int i = 0; i < contacts.length; i++) {
-                          print(i);
-                          await sender.sendSms(new SmsMessage(
-                              contacts[i].number.trim(),
-                              'Help Me ' +
-                                  'http://maps.google.com/?q=' +
-                                  position.latitude.toString() +
-                                  ',' +
-                                  position.longitude.toString()));
-                        }
-                        },
-                        child: Container(
-                          height: 150,
-                          width: 150,
-                          decoration: BoxDecoration(
-                              border: Border.all(color: Colors.white, width: 5),
-                              borderRadius: BorderRadius.circular(300),
-                              color: Colors.red[600]),
-                          child: Center(
+                        SizedBox(width: 20),
+                        SafeArea(
+                          child: InkWell(
+                            onTap: () async {
+                              Position position = await getCurrentLocation();
+                              print("Pressed");
+                              contacts = await dbHelper.getContacts();
+                              print(contacts[0].number);
+                              SmsSender sender = new SmsSender();
+                              for (int i = 0; i < contacts.length; i++) {
+                                print(i);
+                                await sender.sendSms(new SmsMessage(
+                                    contacts[i].number.trim(),
+                                    'Help Me ' +
+                                        'http://maps.google.com/?q=' +
+                                        position.latitude.toString() +
+                                        ',' +
+                                        position.longitude.toString()));
+                              }
+                              Navigator.of(context).pop();
+                            },
                             child: Container(
-                              child: Text("Danger",
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 25,
-                                  )),
+                              height: 150,
+                              width: 150,
+                              decoration: BoxDecoration(
+                                  border:
+                                      Border.all(color: Colors.white, width: 5),
+                                  borderRadius: BorderRadius.circular(300),
+                                  color: Colors.red[600]),
+                              child: Center(
+                                child: Container(
+                                  child: Text("Danger",
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 25,
+                                      )),
+                                ),
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    ),],),
+                      ],
+                    ),
                   ],
                 ),
               ),
